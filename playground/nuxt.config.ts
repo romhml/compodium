97b0fc4c8e42e22fe536d9d722f5aa253c7d1ecf
Romain Hamel <rom.hml@gmail.com>
2025-03-05 16:57:57 +0100

    chore: up

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,16 +1,6 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/ui', '../src/module'],
-  components: [
-    { path: 'components/' },
-    { path: 'my-ui/', prefix: 'D' }
-  ],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  compatibilityDate: '2025-02-12',
-
-  compodium: {
-    excludes: [
-      'InputProps'
-    ]
-  }
+  compatibilityDate: '2025-02-12'
 })
