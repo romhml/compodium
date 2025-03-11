2db8999c8541c54dbc3fd42b1928a4c267293366
Romain Hamel <rom.hml@gmail.com>
2025-03-11 10:09:23 +0100

    chore: remove tailwind resolutions

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,5 +2,13 @@ export default defineNuxtConfig({
   modules: ['@nuxt/ui', 'compodium'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  compatibilityDate: '2025-02-12'
+  compatibilityDate: '2025-02-12',
+
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        skipLibCheck: true
+      }
+    }
+  }
 })
