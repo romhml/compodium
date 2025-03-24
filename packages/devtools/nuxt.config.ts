cef1a20eeae8a4f8e27f184117061b57dd002327
Romain Hamel <rom.hml@gmail.com>
2025-03-24 16:06:14 +0100

    refactor(devtools): enable pre-rendering and remove spaLoadingTemplate

--- packages/devtools/nuxt.config.ts
+++ packages/devtools/nuxt.config.ts
@@ -3,11 +3,11 @@ import { resolve } from 'pathe'
 export default defineNuxtConfig({
   modules: ['@nuxt/ui'],
   ssr: false,
+
   components: {
-    dirs: [
-      { path: 'components', pathPrefix: false }
-    ]
+    dirs: [{ path: 'components', pathPrefix: false }]
   },
+
   devtools: { enabled: false },
 
   app: {
@@ -16,27 +16,18 @@ export default defineNuxtConfig({
       link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }]
     }
   },
+
   css: ['~/assets/css/main.css'],
-  spaLoadingTemplate: true,
+
   future: {
     compatibilityVersion: 4
   },
+
   compatibilityDate: '2024-04-03',
+
   nitro: {
-    hooks: {
-      'prerender:routes': function (routes) {
-        routes.clear()
-      }
-    },
     output: {
       publicDir: resolve(__dirname, '../core/dist/client/devtools')
-    }
-  },
-  vite: {
-    server: {
-      hmr: {
-        clientPort: process.env.PORT ? +process.env.PORT : undefined
-      }
     }
   }
 })
