0f45067f0a74c9d708486d0c6c94d193fa4c32ac
Romain Hamel <rom.hml@gmail.com>
2025-05-13 18:44:49 +0200

    fix(examples): UContainer styles

--- packages/examples/src/examples/ui/ContainerExample.vue
+++ packages/examples/src/examples/ui/ContainerExample.vue
@@ -1,6 +1,6 @@
 <template>
-  <UContainer>
-    <div class="placeholder w-72 aspect-video">
+  <UContainer class="w-72 aspect-video">
+    <div class="placeholder size-full">
       Container
     </div>
   </UContainer>
