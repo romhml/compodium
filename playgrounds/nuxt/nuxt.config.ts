658de1ba5446092d0fa514445d1f5c08ecf67b84
Romain Hamel <rom.hml@gmail.com>
2025-06-20 17:56:22 +0200

    chore: upgrade to nuxt 4

--- playgrounds/nuxt/nuxt.config.ts
+++ playgrounds/nuxt/nuxt.config.ts
@@ -2,7 +2,6 @@ export default defineNuxtConfig({
   modules: ['@nuxt/ui', '../../packages/nuxt/src/module'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  compatibilityDate: '2025-02-12',
 
   typescript: {
     tsConfig: {
