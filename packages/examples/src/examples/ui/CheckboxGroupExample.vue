4d678b9593595eb307752d8b96e87ba20b62e509
Romain Hamel <rom.hml@gmail.com>
2025-06-23 12:06:40 +0200

    feat(examples): add example for CheckboxGroup (`@nuxt/ui`)

---
+++ packages/examples/src/examples/ui/CheckboxGroupExample.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+extendCompodiumMeta({
+  defaultProps: {
+    items: ['Option 1', 'Option 2', 'Option 3']
+  }
+})
+</script>
+
+<template>
+  <UCheckboxGroup />
+</template>
