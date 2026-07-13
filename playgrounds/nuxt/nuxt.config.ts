99dff067d3c8efe95b4a9ab131008d9fe93cd40f
Romain Hamel <romain@rohm.dev>
2026-07-13 17:44:47 +0200

    fix: typechecks

--- playgrounds/nuxt/nuxt.config.ts
+++ playgrounds/nuxt/nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui', '../../packages/nuxt/src'],
+  modules: ['@nuxt/ui', '../../packages/nuxt/src/module'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
 
